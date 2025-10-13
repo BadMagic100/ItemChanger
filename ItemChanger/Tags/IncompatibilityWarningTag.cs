0b12001cf6d2d4df2cb06efd47b12701fe2f346d
Sean Dempsey <dempsey.sean@outlook.com>
2025-10-12 22:45:11 -0700

    Warn about IncompatibilityWarningTag edge case

--- ItemChanger/Tags/IncompatibilityWarningTag.cs
+++ ItemChanger/Tags/IncompatibilityWarningTag.cs
@@ -5,7 +5,10 @@ using ItemChanger.Placements;
 namespace ItemChanger.Tags;
 
 /// <summary>
-/// Tag which generates a warning message on load if a mutually incompatible placement exists.
+/// Tag which generates a warning message on load if an incompatible placement exists.
+/// Generally it is expected that incompatible placements are mutually incompatible; that is,
+/// if A is incompatible with B, then B is also incompatible with A. If this is not satisfied, there
+/// can be edge cases where no warning message is generated.
 /// </summary>
 public class IncompatibilityWarningTag : Tag
 {
@@ -16,7 +19,7 @@ public class IncompatibilityWarningTag : Tag
         string? parentPlacementName = parent switch
         {
             Placement parentPlacement => parentPlacement.Name,
-            Location parentLocation => parentLocation.Placement.Name,
+            Location parentLocation => parentLocation.Placement!.Name,
             _ => null,
         };
 
