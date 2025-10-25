5ebe35824be0d9c33e48ea95365e6a019c998cf0
Sean Dempsey <dempsey.sean@outlook.com>
2025-10-24 21:34:27 -0700

    Implement DebugItem

---
+++ ItemChanger.Core/Items/DebugItem.cs
@@ -0,0 +1,15 @@
+﻿using ItemChanger.Logging;
+
+namespace ItemChanger.Items;
+
+/// <summary>
+/// An item that can be used to quickly test the behavior of a location without requiring full item implementations
+/// </summary>
+public class DebugItem : Item
+{
+    /// <inheritdoc/>
+    public override void GiveImmediate(GiveInfo info)
+    {
+        LoggerProxy.LogInfo($"Given item {Name} with info: {{container={info.Container} fling={info.FlingType} msg={info.MessageType}}}");
+    }
+}
